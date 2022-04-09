8448547f8349a94b74f0f81b571dcd0e39a0eaa0
lordnexen <deathorion@icloud.com>
2022-04-09 20:04:51 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir= os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,'..','adws.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-
